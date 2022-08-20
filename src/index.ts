01770ce34ec27b7dc31340ac93ead57baf0bf8ef
Anhgelus Morhtuuzh <anhgelus.morhtuuzh@gmx.com>
2022-08-20 21:50:34 +0200

    fix new page when clicking on a navbar-link

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,3 @@
 import LevelBar from "./LevelBar.js";
 
 customElements.define('level-bar', LevelBar);
-
-const links = document.querySelectorAll('a');
-const regexBlank = /^https:\/\//;
-links.forEach(link => {
-    regexBlank.test(link.href) ? link.target = '_blank' : link.target = '_self';
-});
