108425c9744f06e02df07e483e686209d8c72771
Anhgelus Morhtuuzh <anhgelus.morhtuuzh@gmx.com>
2022-11-01 21:17:59 +0100

    refactor(project-card) optimization of the code

--- assets/index.js
+++ assets/index.js
@@ -1,5 +1,7 @@
 import LevelBar from "./LevelBar.js";
 import scrollingAnimation from "./scrollingAnimation.js";
+import ProjectCard from "./ProjectCard.js";
 customElements.define('level-bar', LevelBar);
+customElements.define('project-card', ProjectCard);
 scrollingAnimation();
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import LevelBar from "./LevelBar.js";
 import scrollingAnimation from "./scrollingAnimation.js";
+import ProjectCard from "./ProjectCard.js";
 
 customElements.define('level-bar', LevelBar);
+customElements.define('project-card', ProjectCard);
 
 scrollingAnimation();
