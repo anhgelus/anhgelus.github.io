fcc391f91849336b493afa096c6653af218572ed
Anhgelus Morhtuuzh <anhgelus.morhtuuzh@gmx.com>
2023-06-07 20:16:41 +0200

    feat(schools): schools-info custom element

--- assets/schools/SchoolsInfo.js
+++ assets/schools/SchoolsInfo.js
@@ -3,6 +3,14 @@ export default class SchoolsInfo extends HTMLElement {
         super();
     }
     connectedCallback() {
+        const type = this.getAttribute("type");
+        const name = this.getAttribute("name");
+        const city = this.getAttribute("city");
+        this.classList.add("schools", "schools-info");
+        this.innerHTML = `
+        <h4 class="title">${type} - ${name}</h4>
+        <h5 class="subtitle">${city}</h5>
+        `;
     }
 }
 //# sourceMappingURL=SchoolsInfo.js.map
--- src/schools/SchoolsInfo.ts
+++ src/schools/SchoolsInfo.ts
@@ -4,6 +4,13 @@ export default class SchoolsInfo extends HTMLElement {
     }
 
     connectedCallback() {
-
+        const type = this.getAttribute("type")!;
+        const name = this.getAttribute("name")!;
+        const city = this.getAttribute("city")!;
+        this.classList.add("schools", "schools-info");
+        this.innerHTML = `
+        <h4 class="title">${type} - ${name}</h4>
+        <h5 class="subtitle">${city}</h5>
+        `;
     }
 }
