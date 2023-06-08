b3dd0326299c57175f09a8debc4c328f5e1ddd8d
Anhgelus Morhtuuzh <anhgelus.morhtuuzh@proton.me>
2023-06-08 11:05:58 +0200

    feat(schools): toogle content of info

--- src/schools/SchoolsInfo.ts
+++ src/schools/SchoolsInfo.ts
@@ -8,6 +8,7 @@ export default class SchoolsInfo extends HTMLElement {
         const name = this.getAttribute("name")!;
         const city = this.getAttribute("city")!;
         this.classList.add("schools", "schools-info");
+        this.registerEvents();
         if (this.innerHTML != "") {
             this.innerHTML = `
             <h4 class="title">${type} - ${name}</h4>
@@ -20,5 +21,23 @@ export default class SchoolsInfo extends HTMLElement {
         <h4 class="title">${type} - ${name}</h4>
         <h5 class="subtitle">${city}</h5>
         `;
+    }
+
+    registerEvents() {
+        console.log("called")
+        console.log(this)
+        this.addEventListener("click", (e) => {
+            console.log("click")
+            for (let i = 0; i < this.children.length; i++) {
+                const child = this.children[i];
+                if (!(child instanceof HTMLDivElement)) {
+                    continue
+                }
+                if (!child.classList.contains("content")) {
+                    continue
+                }
+                child.classList.toggle("is-enable")
+            }
+        })
     }
 }
