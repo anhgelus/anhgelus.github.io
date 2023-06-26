8a328cf07c84fe82ddc87f66caa327f68440b426
Anhgelus Morhtuuzh <anhgelus.morhtuuzh@gmx.com>
2023-06-26 19:14:05 +0200

    fix(projects): bad presentation

--- src/projects/ProjectsList.ts
+++ src/projects/ProjectsList.ts
@@ -9,14 +9,13 @@ export class ProjectsList extends HTMLElement {
         const href = this.getAttribute('href')!!;
         const projects: Project[] = await fetch(`${window.location.origin}/content/${href}`)
             .then(r => r.json());
-        console.log(projects)
-        let content = ""
+        let content = '<div class="project-list">'
         for (let i = 0; i < projects.length; i++) {
             const project = projects[i];
             content += `<project-card title="${project.title}" href="${project.link}" image-src="${project.image}" image-alt="${project.alt}">`
             content += `<to-get name="projects/${project.content}" simple="yes"></to-get>`;
             content += `</project-card>`;
         }
-        this.innerHTML = content;
+        this.innerHTML = content + "</div>";
     }
 }
