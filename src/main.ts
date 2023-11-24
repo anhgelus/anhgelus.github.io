a9fe5b17315763b15269cbcca9ac46acad8238ca
Anhgelus Morhtuuzh <anhgelus.morhtuuzh@proton.me>
2023-11-24 13:34:56 +0100

    feat(page): general terms of use

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,8 @@ import ToGet from "./ToGet.js";
 import SchoolsDiploma from "./schools/SchoolsDiploma.js";
 import SchoolsInfo from "./schools/SchoolsInfo.js";
 import {ProjectsList} from "./projects/ProjectsList.js";
+import LegalFrench from "./legal/french.js";
+import LegalEnglish from "./legal/english.js";
 
 const projects = document.querySelectorAll<ProjectCard>("project-card");
 
@@ -16,6 +18,9 @@ customElements.define('projects-list', ProjectsList);
 
 customElements.define('schools-diploma', SchoolsDiploma);
 customElements.define('schools-info', SchoolsInfo);
+
+customElements.define('legal-french', LegalFrench);
+customElements.define('legal-english', LegalEnglish);
 
 scrollingAnimation();
 if (projects != null && projects.length > 0) {
@@ -30,3 +35,28 @@ function reveal(e: HTMLElement|null) {
 reveal(document.getElementById("about"));
 reveal(document.getElementById("contact"));
 reveal(document.getElementById("schools"));
+
+function showLegal(hash: string) {
+    const content = document.getElementById("content")!
+    if (hash.endsWith("-fr")) {
+        content.innerHTML = "<legal-french></legal-french>"
+    } else if (hash.endsWith("-en")) {
+        content.innerHTML = "<legal-english></legal-english>"
+    }
+    document.getElementById("header")!.innerHTML = ""
+}
+
+if (window.location.hash.startsWith("#legal")) {
+    showLegal(window.location.hash)
+}
+
+document.querySelectorAll("a").forEach((e) => {
+    const anchor = e as HTMLAnchorElement
+    if (anchor.href.includes(("#legal"))) {
+        console.log(anchor)
+        anchor.addEventListener("click", () => {
+            showLegal(anchor.href.split("#")[1])
+            window.scrollTo({top: 0, behavior: 'smooth'})
+        })
+    }
+})
