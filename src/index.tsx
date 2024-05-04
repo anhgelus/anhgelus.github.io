e7c15db797aba7f8b5884275d52f07e1411ffdef
Anhgelus Morhtuuzh <anhgelus.morhtuuzh@proton.me>
2024-05-04 22:30:55 +0200

    build(): setup tailwind, eslint, prettier and other stuff

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,10 @@
 /* @refresh reload */
-import { render } from 'solid-js/web'
+import { render } from "solid-js/web";
 
-import './index.css'
-import App from './App'
+import "./reset.css";
+import "./index.css";
+import App from "./App";
 
-const root = document.getElementById('root')
+const root = document.getElementById("root")!!;
 
-render(() => <App />, root!)
+render(() => <App />, root!);
