b6125e4da810937e7acff0b41e438113f6999905
Anhgelus Morhtuuzh <anhgelus.morhtuuzh@proton.me>
2024-05-05 22:42:55 +0200

    style(html): add nav, section and main

--- src/App.jsx
+++ src/App.jsx
@@ -5,32 +5,32 @@ import Projects from "./components/Projects.jsx";
 
 function App() {
   return (
-    <>
+    <main>
       <div className="flex h-screen w-full items-center justify-center">
         <div class="fixed" id="animation-p1">
           <Hero title="Anhgelus Morhtuuzh" subtitle="Developer" />
         </div>
       </div>
       <div className="h-screen-half block w-full"></div>
-      <div className="block w-full bg-indigo-950">
-        <div
+      <section className="block w-full bg-indigo-950">
+        <nav
           className="bg-glass relative left-0 top-0 z-50 h-16 w-full"
           id="navbar"
-        ></div>
+        ></nav>
         <div class="py-24" id={"skills"}>
           <Skills />
         </div>
-      </div>
+      </section>
       <div class="divider"></div>
-      <div className="block w-full bg-gradient-to-r from-indigo-900 to-slate-800 py-24">
+      <section className="block w-full bg-gradient-to-r from-indigo-900 to-slate-800 py-24">
         <UniversityCareer />
-      </div>
+      </section>
       <div className="divider"></div>
-      <div className="block w-full bg-slate-800 py-24">
+      <section className="block w-full bg-slate-800 py-24">
         <Projects />
-      </div>
+      </section>
       <div className="divider"></div>
-    </>
+    </main>
   );
 }
 
