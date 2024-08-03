01601e0b186520a69372437056e1370acd64a934
anhgelus <anhgelus.morhtuuzh@proton.me>
2024-08-03 00:19:13 +0000

    feat(header): basic template

---
+++ src/scroll.ts
@@ -0,0 +1,10 @@
+import LocomotiveScroll from 'locomotive-scroll';
+import 'locomotive-scroll/dist/locomotive-scroll.css'
+
+const scroll = new LocomotiveScroll({
+    smooth: true,
+});
+
+export function scrollTo(element: HTMLElement) {
+    scroll.scrollTo(element)
+}
