24aa038b123f391efaf5558e3397d0647ea26600
anhgelus <anhgelus.morhtuuzh@proton.me>
2024-09-14 13:29:33 +0000

    feat(style): change style and use catpuccin colors

--- src/scroll.ts
+++ src/scroll.ts
@@ -1,10 +1,10 @@
 import LocomotiveScroll from 'locomotive-scroll';
 import 'locomotive-scroll/dist/locomotive-scroll.css'
 
-const scroll = new LocomotiveScroll({
-    smooth: true,
-});
+// const scroll = new LocomotiveScroll({
+//     smooth: true,
+// });
 
 export function scrollTo(element: HTMLElement) {
-    scroll.scrollTo(element)
+    // scroll.scrollTo(element)
 }
