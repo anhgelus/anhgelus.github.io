089d93ecea085f0db5e9b2d74e7a4b092915af0d
Anhgelus Morhtuuzh <anhgelus.morhtuuzh@proton.me>
2024-09-18 21:21:27 +0200

    feat(schools): image

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   integrations: [],
-  site: 'https://www.anhgelus.world',
+  site: "https://www.anhgelus.world",
 });
